39ae2904a7988907bcb069d76606ddb55afcfa56
dartunian <dartunian@me.com>
2020-12-23 17:59:19 -0800

    Update MainMenu.vue

--- resources/js/Shared/MainMenu.vue
+++ resources/js/Shared/MainMenu.vue
@@ -2,25 +2,25 @@
   <div>
     <div class="mb-4">
       <inertia-link class="flex items-center group py-3" :href="route('dashboard')">
-        <icon name="dashboard" class="w-4 h-4 mr-2" :class="isUrl('') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
+        <icon name="dashboard" class="w-4 h-4 mr-2" :class="isUrl('') ? 'fill-blue-500' : 'fill-blue-500 group-hover:fill-white'" />
         <div :class="isUrl('') ? 'text-white' : 'text-blue-500 group-hover:text-blue-700'">Dashboard</div>
       </inertia-link>
     </div>
     <div class="mb-4">
       <inertia-link class="flex items-center group py-3" :href="route('organizations')">
-        <icon name="office" class="w-4 h-4 mr-2" :class="isUrl('organizations') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
+        <icon name="office" class="w-4 h-4 mr-2" :class="isUrl('organizations') ? 'fill-blue-500' : 'fill-blue-500 group-hover:fill-white'" />
         <div :class="isUrl('organizations') ? 'text-white' : 'text-blue-500 group-hover:text-blue-700'">Organizations</div>
       </inertia-link>
     </div>
     <div class="mb-4">
       <inertia-link class="flex items-center group py-3" :href="route('contacts')">
-        <icon name="users" class="w-4 h-4 mr-2" :class="isUrl('contacts') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
+        <icon name="users" class="w-4 h-4 mr-2" :class="isUrl('contacts') ? 'fill-blue-500' : 'fill-blue-500 group-hover:fill-white'" />
         <div :class="isUrl('contacts') ? 'text-white' : 'text-blue-500 group-hover:text-blue-700'">Contacts</div>
       </inertia-link>
     </div>
     <div class="mb-4">
       <inertia-link class="flex items-center group py-3" :href="route('reports')">
-        <icon name="printer" class="w-4 h-4 mr-2" :class="isUrl('reports') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
+        <icon name="printer" class="w-4 h-4 mr-2" :class="isUrl('reports') ? 'fill-blue-500' : 'fill-blue-500 group-hover:fill-white'" />
         <div :class="isUrl('reports') ? 'text-white' : 'text-blue-500 group-hover:text-blue-700'">Reports</div>
       </inertia-link>
     </div>
