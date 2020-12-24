e227e271091470758504ac31df778309a866fb88
dartunian <dartunian@me.com>
2020-12-23 17:14:21 -0800

    Update Layout.vue

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -4,7 +4,7 @@
     <div class="flex flex-col">
       <div class="h-screen flex flex-col" @click="hideDropdownMenus">
         <div class="md:flex">
-          <div class="bg-red-900 md:flex-shrink-0 md:w-56 px-6 py-4 flex items-center justify-between md:justify-center">
+          <div class="bg-indigo-900 md:flex-shrink-0 md:w-56 px-6 py-4 flex items-center justify-between md:justify-center">
             <inertia-link class="mt-1" href="/">
               <logo class="fill-white" width="120" height="28" />
             </inertia-link>
