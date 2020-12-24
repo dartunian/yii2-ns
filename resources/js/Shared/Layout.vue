be95d5ef1dc380c04202d6614c394bc0e0a1d8f8
dartunian <dartunian@me.com>
2020-12-23 16:10:50 -0800

    Update Layout.vue

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -9,7 +9,6 @@
               <logo class="fill-white" width="120" height="28" />
             </inertia-link>
             <dropdown class="md:hidden" placement="bottom-end">
-              <svg class="fill-white w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
               <div slot="dropdown" class="mt-2 px-8 py-4 shadow-lg bg-indigo-800 rounded">
                 <main-menu :url="url()" />
               </div>
